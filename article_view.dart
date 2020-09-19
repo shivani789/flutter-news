0a5f540392ee02faee8e549b8cc8328fa5c811ba
Shivani Gupta <53372454+shivani789@users.noreply.github.com>
2020-09-19 16:42:02 +0530

    Create article_view.dart

---
+++ article_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ArticleView extends StatefulWidget{
+  @override
+  _ArticleViewState createState() => _State();
+}
+
+class _ArticleViewState extends State<>{
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
