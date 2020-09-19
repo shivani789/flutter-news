755dc992baa2551d11696a1e2856f7fbaac5ff05
Shivani Gupta <53372454+shivani789@users.noreply.github.com>
2020-09-19 16:42:32 +0530

    Create category_news.dart

---
+++ category_news.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategoryView extends StatefulWidget{
+  @override
+  _CategoryViewState createState() => _State();
+}
+
+class _CategoryViewState extends State<>{
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
